6570fb1e880dc65dd6e3ddcad847443dfc18ec78
seshendramln <seshendrams@gmail.com>
2014-01-19 17:38:40 +0100

    added some details and bugs

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,5 +1,7 @@
 class CompaniesController < ApplicationController
-  before_action :authenticate_user! , :except => [:index]
+  before_action :authenticate_user! , :only => [:show]
+  #, :except => [:index] 
+  
   
 
   def index
@@ -13,38 +15,50 @@ class CompaniesController < ApplicationController
  
   def show
     @company = Company.find(params[:id])
-    company_rating = CompanyRating.where(:company_id =>@company.id)
-    rating_size = company_rating.size
-   
-    #@avg_satisfaction = company_rating.collect(&:satisfaction).sum.to_f/rating_size
-    #@avg_work_environment = company_rating.collect(&:work_environment).sum.to_f/rating_size
-    #@avg_cd = company_rating.collect(&:cd).sum.to_f/rating_size
-    #@avg_flexibility = company_rating.collect(&:flexibility).sum.to_f/rating_size
-    #@avg_relation = company_rating.collect(&:relation).sum.to_f/rating_size
-=begin
-    @avg_leadership = company_rating.collect(&:leadership).sum.to_f/rating_size
-    @avg_pride = company_rating.collect(&:pride).sum.to_f/rating_size
-    @avg_opportunities = company_rating.collect(&:opportunities).sum.to_f/rating_size
-    @avg_opinion = company_rating.collect(&:opinion).sum.to_f/rating_size
-    @avg_stimulatingjob = company_rating.collect(&:stimulatingjob).sum.to_f/rating_size
-    @avg_innovation = company_rating.collect(&:innovation).sum.to_f/rating_size
-    @avg_management= company_rating.collect(&:management).sum.to_f/rating_size
-    @avg_compensation = company_rating.collect(&:compensation).sum.to_f/rating_size
-    @avg_workload = company_rating.collect(&:workload).sum.to_f/rating_size
-    @avg_equality = company_rating.collect(&:equality).sum.to_f/rating_size
-    @avg_objectives= company_rating.collect(&:objectives).sum.to_f/rating_size
-    @avg_cooperation = company_rating.collect(&:cooperation).sum.to_f/rating_size
-    @avg_guidance = company_rating.collect(&:guidance).sum.to_f/rating_size
-    @avg_reward = company_rating.collect(&:reward).sum.to_f/rating_size
-    @avg_career = company_rating.collect(&:career).sum.to_f/rating_size
-    @avg_transparancy = company_rating.collect(&:transparancy).sum.to_f/rating_size
- 
-    @count= company_rating.count
-   @total= (@avg_satisfaction+@avg_work_environment+@avg_cd+@avg_flexibility+@avg_relation+@avg_leadership+@avg_pride+@avg_opportunities+@avg_opinion+@avg_stimulatingjob+@avg_innovation+@avg_management+@avg_compensation+@avg_workload+@avg_equality+@avg_objectives+@avg_cooperation+@avg_guidance+@avg_reward+@avg_career+@avg_transparancy)/21
+    #@article.user_id = current_user.id 
+    company_rating = CompanyRating.where( :company_id =>@company.id,
+                                          :user_id => current_user.id ) 
+    #rating_size = company_rating.size
+
+    
+    @avg_satisfaction = company_rating.collect(&:satisfaction)
+    @avg_work_environment = company_rating.collect(&:work_environment)
+    @avg_cd = company_rating.collect(&:cd)
+    @avg_flexibility = company_rating.collect(&:flexibility)
+    @avg_relation = company_rating.collect(&:relation)
+
+    @avg_leadership = company_rating.collect(&:leadership)
+
+     rating_size6=company_rating.collect(&:pride).count
+    @avg_pride = (company_rating.collect(&:pride))
+    @avg_opportunities = company_rating.collect(&:opportunities)
+    @avg_opinion = company_rating.collect(&:opinion)
+    @avg_stimulatingjob = company_rating.collect(&:stimulatingjob)
+    #@avg_stimulatingjob = (company_rating.collect(&:stimulatingjob).compact.sum.to_i/rating_size if company_rating.blank?)
+    @avg_innovation = company_rating.collect(&:innovation)
+    @avg_management= company_rating.collect(&:management)
+    @avg_compensation = company_rating.collect(&:compensation)
+    @avg_workload = company_rating.collect(&:workload)
+    @avg_equality = company_rating.collect(&:equality)
+    @avg_objectives= company_rating.collect(&:objectives)
+    @avg_cooperation = company_rating.collect(&:cooperation)
+    @avg_guidance = company_rating.collect(&:guidance)
+    @avg_reward = company_rating.collect(&:reward)
+    @avg_career = company_rating.collect(&:career)
+    @avg_transparancy = company_rating.collect(&:transparancy)
+    
+    #arr= [avg_satisfaction,@avg_work_environment,@avg_cd,@avg_flexibility,@avg_relation,@avg_leadership,@avg_pride,@avg_opportunities,@avg_opinion,@avg_stimulatingjob,@avg_innovation,@avg_management,@avg_compensation,@avg_workload,@avg_equality,@avg_objectives,@avg_cooperation,@avg_guidance,@avg_reward,@avg_career,@avg_transparancy]
+    #@current_user_satisfaction = @avg_satisfaction+@avg_work_environment+@avg_cd+@avg_flexibility+@avg_relation+@avg_leadership+@avg_pride+@avg_opportunities+@avg_opinion+@avg_stimulatingjob+@avg_innovation+@avg_management+@avg_compensation+@avg_workload+@avg_equality+@avg_objectives+@avg_cooperation+@avg_guidance+@avg_reward+@avg_career+@avg_transparancy).compact/21
+    arr= @avg_satisfaction+@avg_work_environment+@avg_cd+@avg_flexibility+@avg_relation+@avg_leadership+@avg_pride+@avg_opportunities+@avg_opinion+@avg_stimulatingjob+@avg_innovation+@avg_management+@avg_compensation+@avg_workload+@avg_equality+@avg_objectives+@avg_cooperation+@avg_guidance+@avg_reward+@avg_career+@avg_transparancy
+     @current_user_satisfaction = arr.flatten.compact.sum / arr.flatten.compact.size if arr.flatten.compact.present?
+     #size = arr.compact.size
+    #total= avg_satisfaction+avg_work_environment+avg_cd+avg_flexibility+avg_relation+avg_leadership+avg_pride+avg_opportunities+avg_opinion+avg_stimulatingjob+avg_innovation+avg_management+avg_compensation+avg_workload+avg_equality+avg_objectives+avg_cooperation+avg_guidance+avg_reward+avg_career+avg_transparancy
+   #raise size.inspect 
+   #@total= (@avg_satisfaction+@avg_work_environment+@avg_cd+@avg_flexibility+@avg_relation+@avg_leadership+@avg_pride+@avg_opportunities+@avg_opinion+@avg_stimulatingjob+@avg_innovation+@avg_management+@avg_compensation+@avg_workload+@avg_equality+@avg_objectives+@avg_cooperation+@avg_guidance+@avg_reward+@avg_career+@avg_transparancy).compact/21
   #arr= @avg_satisfaction+@avg_work_environment+@avg_cd+@avg_flexibility+@avg_relation+@avg_leadership+@avg_pride+@avg_opportunities+@avg_opinion+@avg_stimulatingjob+@avg_innovation+@avg_management+@avg_compensation+@avg_workload+@avg_equality+@avg_objectives+@avg_cooperation+@avg_guidance+@avg_reward+@avg_career+@avg_transparancy
    #(company.attrib_satisfaction+company.attrib_work_environment+company.attrib_cd+company.attrib_flexibility+company.attrib_relation+company.attrib_leadership+company.attrib_pride+company.attrib_opportunities+company.attrib_opinion+company.attrib_stimulatingjob+company.attrib_innovation+company.attrib_management+company.attrib_compensation+company.attrib_workload+company.attrib_equality+company.attrib_objectives+company.attrib_cooperation+company.attrib_guidance+company.attrib_reward+company.attrib_career+company.attrib_transparancy)/company.reviews 
-=end 
-    #@total = arr.inject{ |sum, el| sum + el }.to_f / arr.size
+
+    #@total = arr.inject{ |sum, el| sum + el }.to_i / arr.size
   end
 
  
--- app/models/company.rb
+++ app/models/company.rb
@@ -12,94 +12,115 @@ class Company < ActiveRecord::Base
 
   def update_attribscore
   	 rating_size=company_ratings.collect(&:satisfaction).count
-  	 update_attributes(:attrib_satisfaction => company_ratings.collect(&:satisfaction).sum.to_f/rating_size)
-
+     attrb1= (company_ratings.collect(&:satisfaction).compact.sum.to_f/rating_size) 
+  	 update_attributes(:attrib_satisfaction => attrb1)
+ 
      rating_size1=company_ratings.collect(&:work_environment).count
-  	 update_attributes(:attrib_work_environment => company_ratings.collect(&:work_environment).sum.to_f/rating_size1)
+     attrb2= (company_ratings.collect(&:work_environment).compact.sum.to_f/rating_size1 )
+  	 update_attributes(:attrib_work_environment => attrb2)
 
   	 rating_size2=company_ratings.collect(&:cd).count
-  	 update_attributes(:attrib_cd => company_ratings.collect(&:cd).sum.to_f/rating_size2)
+     attrb3= (company_ratings.collect(&:cd).compact.sum.to_f/rating_size2 )
+  	 update_attributes(:attrib_cd => attrb3)
 
   	 rating_size3=company_ratings.collect(&:flexibility).count
-  	 update_attributes(:attrib_flexibility => company_ratings.collect { |x| x.flexibility.to_i}.sum.to_f/rating_size3)
+     attrb4 = (company_ratings.collect { |x| x.flexibility.to_i}.sum.to_f/rating_size3 )
+  	 update_attributes(:attrib_flexibility => attrb4)
 
   	 rating_size4=company_ratings.collect(&:relation).count
-  	 update_attributes(:attrib_relation => company_ratings.collect(&:relation).compact.sum.to_f/rating_size4)
+     attrb5=(company_ratings.collect(&:relation).compact.sum.to_f/rating_size4 )
+  	 update_attributes(:attrib_relation => attrb5)
 
   	 rating_size5=company_ratings.collect(&:leadership).count
-  	 update_attributes(:attrib_leadership => company_ratings.collect(&:leadership).sum.to_f/rating_size5)
+     attrb6=(company_ratings.collect(&:leadership).compact.sum.to_f/rating_size5 ) 
+  	 update_attributes(:attrib_leadership => attrb6 )
 
   	 rating_size6=company_ratings.collect(&:pride).count
-  	 update_attributes(:attrib_pride => company_ratings.collect(&:pride).compact.sum.to_f/rating_size6)
+     attrb7= (company_ratings.collect(&:pride).compact.sum.to_f/rating_size6 )
+  	 update_attributes(:attrib_pride => attrb7)
 
   	 rating_size7=company_ratings.collect(&:opportunities).count
-  	 update_attributes(:attrib_opportunities => company_ratings.collect(&:opportunities).compact.sum.to_f/rating_size7)
+     attrb8=(  company_ratings.collect(&:opportunities).compact.sum.to_f/rating_size7 )
+  	 update_attributes(:attrib_opportunities =>attrb8  )
 
   	 
   	 rating_size8=company_ratings.collect(&:opinion).count
-  	 update_attributes(:attrib_opinion => company_ratings.collect(&:opinion).compact.sum.to_f/rating_size8)
+     attrb9=(company_ratings.collect(&:opinion).compact.sum.to_f/rating_size8 )
+  	 update_attributes(:attrib_opinion => attrb9)
 
   	 rating_size9=company_ratings.collect(&:stimulatingjob).count
-  	 update_attributes(:attrib_stimulatingjob => company_ratings.collect(&:stimulatingjob).compact.sum.to_f/rating_size9)
+     attrb10=(company_ratings.collect(&:stimulatingjob).compact.sum.to_f/rating_size9 )
+  	 update_attributes(:attrib_stimulatingjob => attrb10 )
 
   	 rating_size10=company_ratings.collect(&:innovation).count
-  	 update_attributes(:attrib_innovation => company_ratings.collect(&:innovation).compact.sum.to_f/rating_size10)
+     attrb11=(company_ratings.collect(&:innovation).compact.sum.to_f/rating_size10 )
+  	 update_attributes(:attrib_innovation => attrb11 )
 
   	 rating_size11=company_ratings.collect(&:management).count
-  	 update_attributes(:attrib_management => company_ratings.collect(&:management).compact.sum.to_f/rating_size11)
+     attrb12=(company_ratings.collect(&:management).compact.sum.to_f/rating_size11 )
+  	 update_attributes(:attrib_management => attrb12)
 
   	 rating_size12=company_ratings.collect(&:compensation).count
-  	 update_attributes(:attrib_compensation => company_ratings.collect(&:compensation).compact.sum.to_f/rating_size12)
+     attrb13=(company_ratings.collect(&:compensation).compact.sum.to_f/rating_size12 )
+  	 update_attributes(:attrib_compensation => attrb13)
 
   	 rating_size13=company_ratings.collect(&:workload).count
-  	 update_attributes(:attrib_workload=> company_ratings.collect(&:workload).compact.sum.to_f/rating_size13)
+     attrb14=(company_ratings.collect(&:workload).compact.sum.to_f/rating_size13 )
+  	 update_attributes(:attrib_workload=> attrb14)
 
   	 rating_size14=company_ratings.collect(&:equality).count
-  	 update_attributes(:attrib_equality => company_ratings.collect(&:equality).compact.sum.to_f/rating_size14)
+     attrb15=(company_ratings.collect(&:equality).compact.sum.to_f/rating_size14 )
+  	 update_attributes(:attrib_equality => attrb15)
 
   	 rating_size15=company_ratings.collect(&:objectives).count
-  	 update_attributes(:attrib_objectives => company_ratings.collect(&:objectives).compact.sum.to_f/rating_size15)
+     attrb16=(company_ratings.collect(&:objectives).compact.sum.to_f/rating_size15 )
+  	 update_attributes(:attrib_objectives => attrb16)
 
   	 rating_size16=company_ratings.collect(&:cooperation).count
-  	 update_attributes(:attrib_cooperation => company_ratings.collect(&:cooperation).compact.sum.to_f/rating_size16)
+     attrb17=(company_ratings.collect(&:cooperation).compact.sum.to_f/rating_size16 )
+  	 update_attributes(:attrib_cooperation => attrb17 )
 
 
   	 rating_size17=company_ratings.collect(&:guidance).count
-  	 update_attributes(:attrib_guidance => company_ratings.sum("guidance").to_f/rating_size17)
-     #update_attributes(:attrib_guidance => company_ratings.collect(&:guidance).compact.sum.to_f/rating_size17)
+     attrb18=(company_ratings.sum("guidance").to_f/rating_size17 )
+  	 update_attributes(:attrib_guidance => attrb18)
+     #update_attributes(:attrib_guidance => (  company_ratings.collect(&:guidance).compact.sum.to_f/rating_size17)
 
   	 rating_size18=company_ratings.collect(&:reward).count
-  	 update_attributes(:attrib_reward => company_ratings.collect(&:reward).compact.sum.to_f/rating_size18)
+     attrb19=(company_ratings.collect(&:reward).compact.sum.to_f/rating_size18 )
+  	 update_attributes(:attrib_reward =>  attrb19)
 
 
   	 rating_size19=company_ratings.collect(&:career).count
-  	 update_attributes(:attrib_career => company_ratings.collect(&:career).compact.sum.to_f/rating_size19)
+     attrb20=(company_ratings.collect(&:career).compact.sum.to_f/rating_size19 )
+  	 update_attributes(:attrib_career => attrb20 )
 
 
   	 rating_size20=company_ratings.collect(&:transparancy).count
-  	 #attrib1= company_ratings.collect(&:transparancy).sum.to_f/rating_size20
-  	 update_attributes(:attrib_transparancy=> company_ratings.collect(&:transparancy).compact.sum.to_f/rating_size20)
-     
+  	 attrb21= (  company_ratings.collect(&:transparancy).compact.sum.to_f/rating_size20  )
+  	 update_attributes(:attrib_transparancy=> attrb21 )
+     #raise attrb3.inspect
      review= rating_size+rating_size1+rating_size2+rating_size3+rating_size4+rating_size5+rating_size6+rating_size7+rating_size8+rating_size9+rating_size10+rating_size11+rating_size12+rating_size13+rating_size14+rating_size15+rating_size16+rating_size17+rating_size18+rating_size19+rating_size20
-     update_attributes(:reviews => review)
+     update_attributes(:reviews =>   review)
      
      totalreview = company_ratings.count
      update_attributes(:totalreviews => totalreview)
   	# raise rating_size.inspect
       update_satisfaction
       update_rank
+
   end
 
   #after_update :update_satisfaction
   
  
   def update_satisfaction
-  	 #review= self.reviews
-  	 
-  	 update_attributes(:satisfaction => ( attrib_satisfaction.to_i+attrib_work_environment.to_i+attrib_cd.to_i+attrib_flexibility.to_i++attrib_relation.to_i+attrib_leadership.to_i+attrib_pride.to_i+attrib_opportunities.to_i+attrib_opinion.to_i+attrib_stimulatingjob.to_i+attrib_innovation.to_i+attrib_management.to_i+attrib_compensation.to_i+attrib_workload.to_i+attrib_equality.to_i+attrib_objectives.to_i+attrib_cooperation.to_i+attrib_guidance+attrib_reward.to_i+attrib_career.to_i+attrib_transparancy.to_i)/self.reviews)
+  	 review= self.reviews
+  	 #raise review.inspect
+  	 update_attributes(:satisfaction => (attrib_satisfaction.to_i+attrib_work_environment.to_i+attrib_cd.to_i+attrib_flexibility.to_i+attrib_relation.to_i+attrib_leadership.to_i+attrib_pride.to_i+attrib_opportunities.to_i+attrib_opinion.to_i+attrib_stimulatingjob.to_i+attrib_innovation.to_i+attrib_management.to_i+attrib_compensation.to_i+attrib_workload.to_i+attrib_equality.to_i+attrib_objectives.to_i+attrib_cooperation.to_i+attrib_guidance.to_i+attrib_reward.to_i+attrib_career.to_i+attrib_transparancy.to_i)/self.reviews)
      #update_attributes(:reviews => company_ratings.count)
      #
-     #raise review.inspect
+     
   end
 	 
 
